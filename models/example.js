6cd7b105cc1208b579bccac3e2c87694c243001a
sadiyaj <sadiyajiru@gmail.com>
2018-10-23 17:40:01 -0500

    created budget model

--- models/example.js
+++ models/example.js
@@ -1,7 +1,44 @@
 module.exports = (sequelize, DataTypes) => {
-  const Example = sequelize.define('Example', {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT,
-  });
-  return Example;
+  const budget = sequelize.define('budget', {
+
+    //total monthly income
+    monthly_icome: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //i.e student loans
+    loan: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+    },
+    //housing cost
+  rent: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  //electric, gas, water
+  utilities: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  //gas, insurance, car note
+  transportation: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  mobile: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  //includes groceries 
+  food: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    
+  },
+
+});
+
+  return budget;
 };
