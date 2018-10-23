7800f95b2a8024001bff70ac775f83a140582fbb
sadiyaj <sadiyajiru@gmail.com>
2018-10-23 18:12:03 -0500

    fixed typo added validations

--- models/example.js
+++ models/example.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     monthly_income: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 0,
     },
     //i.e student loans
     loan: {
@@ -16,29 +17,37 @@ module.exports = (sequelize, DataTypes) => {
   rent: {
     type: DataTypes.INTEGER,
     allowNull: true,
+    defaultValue: 0,
   },
   //electric, gas, water
   utilities: {
     type: DataTypes.INTEGER,
     allowNull: true,
+    defaultValue: 0,
   },
   //gas, insurance, car note
   transportation: {
     type: DataTypes.INTEGER,
     allowNull: true,
+    defaultValue: 0,
   },
   mobile: {
     type: DataTypes.INTEGER,
     allowNull: true,
+    defaultValue: 0,
   },
   //includes groceries 
   food: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    defaultValue: 0,
     
   },
 
 });
+// Syncs with DB
+// budget.sync();
+// Makes the budeget model available for other files (will also create a table)
 
   return budget;
 };
