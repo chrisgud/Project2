b0b0a5e900dabd2ef1f45757703deb67aaf85021
srandle93 <randle.symone.l@gmail.com>
2018-10-24 17:11:12 -0500

    Contining troubleshooting testing.

--- public/js/index.js
+++ public/js/index.js
@@ -1,104 +1,104 @@
-/* eslint-env node, jquery, browser */
-// Get references to page elements
-// Reference to create new bill with button
-const $newbill = $('#newbill');
-// Reference for the div containing the list of bills from the DB
-const $bills = $('#bills');
-// Reference for a delete bill option
-const $deletebill = $('#deletebill');
-// reference for an update bill amount option
-const $updatebill = $('#updatebill');
+// /* eslint-env node, jquery, browser */
+// // Get references to page elements
+// // Reference to create new bill with button
+// const $newbill = $('#newbill');
+// // Reference for the div containing the list of bills from the DB
+// const $bills = $('#bills');
+// // Reference for a delete bill option
+// const $deletebill = $('#deletebill');
+// // reference for an update bill amount option
+// const $updatebill = $('#updatebill');
 
-// The API object contains methods for each kind of request we'll make
-const API = {
-  saveExample(example) {
-    return $.ajax({
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      type: 'POST',
-      url: 'api/examples',
-      data: JSON.stringify(example),
-    });
-  },
-  getExamples() {
-    return $.ajax({
-      url: 'api/examples',
-      type: 'GET',
-    });
-  },
-  deleteExample(id) {
-    return $.ajax({
-      url: `api/examples/${id}`,
-      type: 'DELETE',
-    });
-  },
-};
+// // The API object contains methods for each kind of request we'll make
+// const API = {
+//   saveExample(example) {
+//     return $.ajax({
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       type: 'POST',
+//       url: 'api/examples',
+//       data: JSON.stringify(example),
+//     });
+//   },
+//   getExamples() {
+//     return $.ajax({
+//       url: 'api/examples',
+//       type: 'GET',
+//     });
+//   },
+//   deleteExample(id) {
+//     return $.ajax({
+//       url: `api/examples/${id}`,
+//       type: 'DELETE',
+//     });
+//   },
+// };
 
-// refreshExamples gets new examples from the db and repopulates the list
-const refreshExamples = () => {
-  API.getExamples().then((data) => {
-    const $examples = data.map((example) => {
-      const $a = $('<a>')
-        .text(example.text)
-        .attr('href', `/example/${example.id}`);
+// // refreshExamples gets new examples from the db and repopulates the list
+// const refreshExamples = () => {
+//   API.getExamples().then((data) => {
+//     const $examples = data.map((example) => {
+//       const $a = $('<a>')
+//         .text(example.text)
+//         .attr('href', `/example/${example.id}`);
 
-      const $li = $('<li>')
-        .attr({
-          class: 'list-group-item',
-          'data-id': example.id,
-        })
-        .append($a);
+//       const $li = $('<li>')
+//         .attr({
+//           class: 'list-group-item',
+//           'data-id': example.id,
+//         })
+//         .append($a);
 
-      const $button = $('<button>')
-        .addClass('btn btn-danger float-right delete')
-        .text('ｘ');
+//       const $button = $('<button>')
+//         .addClass('btn btn-danger float-right delete')
+//         .text('ｘ');
 
-      $li.append($button);
+//       $li.append($button);
 
-      return $li;
-    });
+//       return $li;
+//     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+//     $exampleList.empty();
+//     $exampleList.append($examples);
+//   });
+// };
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-const handleFormSubmit = (event) => {
-  event.preventDefault();
+// // handleFormSubmit is called whenever we submit a new example
+// // Save the new example to the db and refresh the list
+// const handleFormSubmit = (event) => {
+//   event.preventDefault();
 
-  const example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim(),
-  };
-  // Alerts are terrible but I'm lazy about changing this placeholder
-  if (!(example.text && example.description)) {
-    alert('You must enter an example text and description!'); //eslint-disable-line
-    return;
-  }
+//   const example = {
+//     text: $exampleText.val().trim(),
+//     description: $exampleDescription.val().trim(),
+//   };
+//   // Alerts are terrible but I'm lazy about changing this placeholder
+//   if (!(example.text && example.description)) {
+//     alert('You must enter an example text and description!'); //eslint-disable-line
+//     return;
+//   }
 
-  API.saveExample(example).then(() => {
-    refreshExamples();
-  });
+//   API.saveExample(example).then(() => {
+//     refreshExamples();
+//   });
 
-  $exampleText.val('');
-  $exampleDescription.val('');
-};
+//   $exampleText.val('');
+//   $exampleDescription.val('');
+// };
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-const handleDeleteBtnClick = () => {
-  const idToDelete = $(this)
-    .parent()
-    .attr('data-id');
+// // handleDeleteBtnClick is called when an example's delete button is clicked
+// // Remove the example from the db and refresh the list
+// const handleDeleteBtnClick = () => {
+//   const idToDelete = $(this)
+//     .parent()
+//     .attr('data-id');
 
-  API.deleteExample(idToDelete).then(() => {
-    refreshExamples();
-  });
-};
+//   API.deleteExample(idToDelete).then(() => {
+//     refreshExamples();
+//   });
+// };
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on('click', handleFormSubmit);
-$exampleList.on('click', '.delete', handleDeleteBtnClick);
+// // Add event listeners to the submit and delete buttons
+// $submitBtn.on('click', handleFormSubmit);
+// $exampleList.on('click', '.delete', handleDeleteBtnClick);
