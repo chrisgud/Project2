0d609449dab12ab4765bff0d6de736034563cdc2
Thomas Winters <winterst85@gmail.com>
2018-10-24 18:46:53 -0500

    added html db

--- config/config.js
+++ config/config.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    host: "localhost",
-    user: "root",
+    host: 'localhost',
+    user: 'root',
     password: null,
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: 'mysql',
+    operatorsAliases: false,
   },
   test: {
-    host: "localhost",
-    dialect: "mysql",
+    host: 'localhost',
+    dialect: 'mysql',
     logging: false,
-    operatorsAliases: false
+    operatorsAliases: false,
   },
   production: {
-    use_env_variable: "JAWSDB_URL",
-    dialect: "mysql"
-  }
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+  },
 };
